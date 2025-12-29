bf441818c2fe6ddb8f33a3e8336a6749d3ee2284
João Lima <jplima1853@gmail.com>
2025-12-29 20:10:42 -0300

    Criação da Aplicação de persistência

---
+++ PicpayChal.App/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicpayChal.App.Data.Maps;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Wallet> Wallets { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new WalletMap());
+        modelBuilder.ApplyConfiguration(new TransactionMap());
+        base.OnModelCreating(modelBuilder);
+    }
+}
