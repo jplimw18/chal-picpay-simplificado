0666df38476bee0b5806185b32b49ee9e6d0ff5c
João Lima <jplima1853@gmail.com>
2025-12-29 19:48:29 -0300

    Mappers para configurar geração de tabelas no EF Core

---
+++ PicpayChal.App/Data/Maps/TransactionMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Data.Maps;
+
+public sealed class TransactionMap : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+        builder.HasOne(x => x.Payer)
+        .WithMany()
+        .HasForeignKey(x => x.PayerId);
+
+
+        builder.HasOne(x => x.Payee)
+        .WithMany()
+        .HasForeignKey(x => x.PayeeId);
+
+
+        builder.Property(x => x.Value).IsRequired();
+    }
+}
---
+++ PicpayChal.App/Data/Maps/WalletMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Data.Maps;
+
+public sealed class WalletMap : IEntityTypeConfiguration<Wallet>
+{
+    public void Configure(EntityTypeBuilder<Wallet> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+
+        builder.Property(x => x.Name).IsRequired();
+
+
+        builder.HasIndex(x => x.Cpf).IsUnique();
+        builder.Property(x => x.Cpf).IsRequired();
+
+
+        builder.HasIndex(x => x.Email).IsUnique();
+        builder.Property(x => x.Email).IsRequired();
+
+
+        builder.Property(x => x.Balance);
+    }
+}
