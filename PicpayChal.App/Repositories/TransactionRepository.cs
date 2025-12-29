78af67539f548b434403a76e2da07204427c8a6d
João Lima <jplima1853@gmail.com>
2025-12-29 20:10:01 -0300

    Repositórios para descrever operações para respectivas entidades

---
+++ PicpayChal.App/Repositories/TransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿using PicpayChal.App.Data;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Repositories;
+
+public sealed class TransactionRepository : BaseRepository<Transaction>
+{
+    public TransactionRepository(AppDbContext context)
+        : base(context) { }
+}
---
+++ PicpayChal.App/Repositories/WalletRepository.cs
@@ -0,0 +1,10 @@
+﻿using PicpayChal.App.Data;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Repositories;
+
+public sealed class WalletRepository : BaseRepository<Wallet>
+{
+    public WalletRepository(AppDbContext context)
+        : base(context) { }
+}
