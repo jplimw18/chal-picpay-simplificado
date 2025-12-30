e1ed87940058b6998e13fd39480cbabeb05185fb
João Lima <jplima1853@gmail.com>
2025-12-29 21:10:07 -0300

    Exceção personalizada para tratamento centralizado de erros referentes a transação

---
+++ PicpayChal.App/Exceptions/InvalidTransactionException.cs
@@ -0,0 +1,7 @@
+﻿namespace PicpayChal.App.Exceptions;
+
+public sealed class InvalidTransactionException : Exception
+{
+    public InvalidTransactionException(string message)
+        : base(message) {  }
+}
