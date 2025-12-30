a3274c46510a34a0eb3d99f039bd68f1aa0b74d4
João Lima <jplima1853@gmail.com>
2025-12-29 21:04:29 -0300

    ajuste de namespaces

--- PicpayChal.App/Repositories/TransactionRepository.cs
+++ PicpayChal.App/Repositories/TransactionRepository.cs
@@ -1,9 +1,10 @@
 ﻿using PicpayChal.App.Data;
 using PicpayChal.App.Entities;
+using PicpayChal.App.Repositories.Interfaces;
 
 namespace PicpayChal.App.Repositories;
 
-public sealed class TransactionRepository : BaseRepository<Transaction>
+public sealed class TransactionRepository : BaseRepository<Transaction>, ITransactionRepository
 {
     public TransactionRepository(AppDbContext context)
         : base(context) { }
